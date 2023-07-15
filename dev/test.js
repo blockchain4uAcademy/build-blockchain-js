bdffd29559177bc5759daa595ef1d10f5ca02f40
khadhraouikhalil <khadhraouikhalil52@gmail.com>
2023-07-15 23:44:47 +0100

    create new block:function && test

--- dev/test.js
+++ dev/test.js
@@ -1,1 +1,9 @@
-consle.log()
+const Blockchain = require('./blockchain');
+
+const bitcoin =new Blockchain();
+
+bitcoin.createNewBlock(2113,'FKG65456GDDG','FQFJK56867');
+bitcoin.createNewBlock(111,'FKG65456GDDG','FQFJK56867');
+bitcoin.createNewBlock(21,'FKG65456GDDG','FQFJK56867');
+
+console.log(bitcoin);

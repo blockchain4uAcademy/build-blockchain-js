ea3b28d666fd9c51425d5dafd595b67184b29e0c
khadhraouikhalil <khadhraouikhalil52@gmail.com>
2023-07-19 15:44:30 +0100

    create new transaction && testing part

--- dev/test.js
+++ dev/test.js
@@ -1,9 +1,23 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin =new Blockchain();
+// testing create block
+/* bitcoin.createNewBlock(2113,'FKG65456GDDG','FQFJK56867'); */
 
+
+// testing create transaction 
 bitcoin.createNewBlock(2113,'FKG65456GDDG','FQFJK56867');
-bitcoin.createNewBlock(111,'FKG65456GDDG','FQFJK56867');
-bitcoin.createNewBlock(21,'FKG65456GDDG','FQFJK56867');
+bitcoin.createNewTransaction(200,'med5456GDDG','KHALILJK56867');
+bitcoin.createNewBlock(156,'FKG65456GDDG','FQFJK56867');
+
+bitcoin.createNewTransaction(200,'med5456GDDG','KHALILJK56867');
+bitcoin.createNewTransaction(200,'med5456GDDG','KHALILJK56867');
+bitcoin.createNewTransaction(200,'med5456GDDG','KHALILJK56867');
+
+
+bitcoin.createNewBlock(156,'FKG65456GDDG','FQFJK56867');
+
 
 console.log(bitcoin);
+
+console.log(bitcoin.chain[1]);

dbe5ca05b69dba2b66c2a82b4938ebac7cf96580
khadhraouikhalil <khadhraouikhalil52@gmail.com>
2023-07-19 20:35:50 +0100

    proof of work  && testing part

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,9 @@
+const sha256 = require('sha256'); 
 function Blockchain() {
   this.chain= [];
   this.pendingTransactions =[];
+  //Create Genesis Block
+  this.createNewBlock(0,'0','0')
 }
 
 Blockchain.prototype.createNewBlock = function(nonce,previousBlockHash,hash){
@@ -34,6 +37,24 @@ Blockchain.prototype.createNewTransaction = function (amount , sender , recipien
 return this.getLastBlock()['index']+1;
 }
 
+Blockchain.prototype.hashBlock = function (previousBlockHash,currentBlockData,nonce ) {
+  const dataAsString=  previousBlockHash+ nonce.toString()+JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+  return hash
+}
+
+
+Blockchain.prototype.proofOfWork =function (previousBlockHash,currentBlockData){
+  let nonce = 0;
+  let hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+  while (hash.substring(0,4)!=="0000") {
+    nonce++;
+    hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+    //console.log(hash);
+  }
+
+  return  nonce;
+}
 
 
 module.exports= Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,23 +1,74 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin =new Blockchain();
-// testing create block
+//------------------01 Testing create block
 /* bitcoin.createNewBlock(2113,'FKG65456GDDG','FQFJK56867'); */
 
 
-// testing create transaction 
-bitcoin.createNewBlock(2113,'FKG65456GDDG','FQFJK56867');
+//------------------02Testing create transaction 
+/* bitcoin.createNewBlock(2113,'FKG65456GDDG','FQFJK56867');
 bitcoin.createNewTransaction(200,'med5456GDDG','KHALILJK56867');
 bitcoin.createNewBlock(156,'FKG65456GDDG','FQFJK56867');
 
-bitcoin.createNewTransaction(200,'med5456GDDG','KHALILJK56867');
+bitcoin.createNewTransaction(200,'med5456G DDG','KHALILJK56867');
 bitcoin.createNewTransaction(200,'med5456GDDG','KHALILJK56867');
 bitcoin.createNewTransaction(200,'med5456GDDG','KHALILJK56867');
 
 
-bitcoin.createNewBlock(156,'FKG65456GDDG','FQFJK56867');
+bitcoin.createNewBlock(156,'FKG65456GDDG','FQFJK56867'); */
+//console.log(bitcoin);
+//console.log(bitcoin.chain[1]);
 
+//-----------------03Testing hash block method
+/* const previousBlockHash="5654BFDD86DSF86";
+const currentBlockData= [
+  {
+    amount:1,
+    sender:"IOFOISD545454",
+    recipient:"FOIDUOI545645",
+  },
+  {
+    amount:120,
+    sender:"IOkkFOISD545454",
+    recipient:"OIDUOI545645",
+  }
+,  {
+  amount:200,
+  sender:"FOISD545454",
+  recipient:"DUOI545645",
+}
+
+]
+const nonce=120
+
+const hash =bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce)
+console.log(hash);
+*/
+
+//-----------------04Testing proof of work method
+/* const previousBlockHash="5654BFDD86DSF86";
+const currentBlockData= [
+  {
+    amount:1,
+    sender:"IOFOISD545454",
+    recipient:"FOIDUOI545645",
+  },
+  {
+    amount:120,
+    sender:"IOkkFOISD545454",
+    recipient:"OIDUOI545645",
+  }
+,  {
+  amount:200,
+  sender:"FOISD545454",
+  recipient:"DUOI545645",
+}
+
+]
+const proofedNonce =bitcoin.proofOfWork(previousBlockHash,currentBlockData)
+console.log(proofedNonce);
+
+const hash =bitcoin.hashBlock(previousBlockHash,currentBlockData,proofedNonce)
+console.log(hash); */
 
 console.log(bitcoin);
-
-console.log(bitcoin.chain[1]);

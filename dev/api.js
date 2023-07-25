7ad4a41d88775a5f1b1368c324d1e77174b5e247
khadhraouikhalil <khadhraouikhalil52@gmail.com>
2023-07-25 22:27:10 +0100

    mine api

--- dev/api.js
+++ dev/api.js
@@ -23,6 +23,33 @@ app.get('/blockchain', function (req, res) {
 });
 
 
+// create a new transaction
+app.post('/transaction', function(req, res) {
+	const newTransaction = req.body;
+	const blockIndex = bitcoin.addTransactionToPendingTransactions(newTransaction);
+	res.json({ note: `Transaction will be added in block ${blockIndex}.` });
+});
+
+
+
+
+// mine a block
+app.get('/mine', function(req, res) {
+	const lastBlock = bitcoin.getLastBlock();
+	const previousBlockHash = lastBlock['hash'];
+	const currentBlockData = {
+		transactions: bitcoin.pendingTransactions,
+		index: lastBlock['index'] + 1
+	};
+	const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+	const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+	const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+  res.json({
+    note: "New block mined & broadcast successfully",
+    block: newBlock
+  });
+
+});
 
 app.listen(port, function() {
 	console.log(`Listening on port ${port}...`);
